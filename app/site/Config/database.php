4bc3b0987ccffec8abdae44d48f1dc2444649ae4
felipemineir0 <felipemineir0@hotmail.com>
2019-11-28 13:31:30 -0300

    last commit

--- app/site/Config/database.php
+++ app/site/Config/database.php
@@ -4,6 +4,21 @@ class DATABASE_CONFIG {
     public $default = array(
         'datasource' => 'Database/Mysql',
         'persistent' => false,
+        'host' => 'localhost',
+        'login' => 'id11237991_responde_enem',
+        'password' => 're5p0Nd33NEW!9',
+        'database' => 'id11237991_responde_enem',
+        'prefix' => 'tb_',
+        'encoding' => 'utf8',
+    );
+}
+/*
+<?php
+class DATABASE_CONFIG {
+
+    public $default = array(
+        'datasource' => 'Database/Mysql',
+        'persistent' => false,
         'host' => 'responde_enem.mysql.dbaas.com.br',
         'login' => 'responde_enem',
         'password' => 'Fel!ndw321',
@@ -12,3 +27,4 @@ class DATABASE_CONFIG {
         'encoding' => 'utf8',
     );
 }
+*/
